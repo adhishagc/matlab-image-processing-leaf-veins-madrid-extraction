98c0bfd35c194ba4cef1677a608ff65f6d8764a8
Adhisha <adhishagc@gmail.com>
2019-06-12 00:55:25 +0530

    Jack tree Roberts works

--- source.m
+++ source.m
@@ -10,7 +10,16 @@ IBin = im2bw(IGray,141/255);
 
 
 ed = edge(IBin,'roberts');
-ed_neg = imcomplement(ed);
+
 %ed_neg = medfilt2(ed_neg,[3 3]); % doesnt seem to do well
 
-imshow(ed_neg);
+%structuring
+se = strel('disk',3);
+imdil = imdilate(ed,se);
+imdil_neg = imcomplement(imdil);
+ed_neg = imcomplement(ed);
+
+subplot(1,2,1),
+imshow(ed_neg),
+subplot(1,2,2),
+imshow(imdil_neg)
