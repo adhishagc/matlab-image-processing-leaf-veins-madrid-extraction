191691df0833f97703dbaf7a7d15c522dc138c38
Adhisha <adhishagc@gmail.com>
2019-06-12 20:50:45 +0530

    Attempt General

--- general.m
+++ general.m
@@ -9,20 +9,18 @@ IGrayOri = IGray; %backup the variable
 %Image enhancement using histogram equalization
 IGray = histeq(IGray);
 %image instensity adjust
-IGray = imadjust(IGray,[120/255 130/255]);
+IGray = imadjust(IGray,[130/255 135/255]);
 %imshow(IGray);
 
 %convert to binary image
 IBin = im2bw(IGray,129/255); %141/255 disk 3 initially
 
 %dilate (Not in use)
-se2 = strel('disk',1); %strel('line',3,45); doesnt count the midrib
-IGray = imdilate(IBin,se2);
-
-
+%se2 = strel('line',3,45); %strel('line',3,45); doesnt count the midrib
+%IGray = imdilate(IBin,se2);
 
 %edge detection filter
-ed = edge(IBin,'roberts');
+ed = edge(IBin,'canny');
 %imshow(ed);
 
 
@@ -32,22 +30,13 @@ ed = edge(IBin,'roberts');
 %dilating the image
 se = strel('disk',1); %strel('line',3,45); doesnt count the midrib
 imdil = imdilate(ed,se);
+imdil = medfilt2(imdil,[3 3]);
 %removing small objects in the space
-imdil = bwareaopen(imdil, 900);
+%imdil = bwareaopen(imdil, 300);
 
 imdil_neg = imcomplement(imdil);
-imshow(imdil_neg);
-%ed_neg = imcomplement(ed); good outputs
-%ed_neg = bwareaopen(ed_neg, 50);
+%imshow(imdil_neg);
 
-%noise
-%ed_neg_nf = medfilt2(ed_neg,[3 3]);
-%ed_neg_nf = imerode(ed_neg_nf,se);
-%ed_neg_nf = bwareaopen(ed_neg_nf, 20);
-%ed_neg_nf = medfilt2(ed_neg,[3 3]);
-%ed_neg_nf = imcomplement(ed_neg_nf);
-%ed_neg_nf = imdilate(ed_neg_nf,se);
-%ed_neg_nf = imcomplement(ed_neg_nf);
 
 %get dimensions of the image
 [r,c] = size(IGray);
@@ -61,7 +50,7 @@ for i=1:r
     end
 end
 
-%imshow(imdil_neg)
+imtool(imdil_neg);
 
 %subplot(1,2,1),
 %imshow(ed_neg_nf),
